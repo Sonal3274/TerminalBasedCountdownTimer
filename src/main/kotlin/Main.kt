966575553fd13ea22e2bafb697c1e441aca7d50c
Sonal <sonal.dhage26@gmail.com>
2023-06-13 22:59:19 +0100

    display the current timestamp and take user input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.util.Scanner
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    // Display the current time
+        val currentDateTime = LocalDateTime.now()
+        val formattedDateTime = currentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        println("Current time: $formattedDateTime")
+
+        // Take time input from the user
+        val scanner = Scanner(System.`in`)
+
+        print("Enter a time (HH:mm:ss): ")
+        val userInput = scanner.nextLine()
+
+        val userTime = try {
+            LocalTime.parse(userInput, DateTimeFormatter.ofPattern("HH:mm:ss"))
+        } catch (e: DateTimeParseException) {
+            println("Invalid time format. Please enter time in HH:mm:ss format.")
+            return
+        }
+
+        println("Entered time: $userTime")
+
+
 }
